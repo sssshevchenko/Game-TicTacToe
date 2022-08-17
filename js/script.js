8e316f79272a7443fe2878f2fd63423519d2860e
DenisShevchenko <81032668+sssshevchenko@users.noreply.github.com>
2022-08-17 14:13:24 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 let game = document.querySelector('.game');
-let	res = document.querySelector('.res');
-let	btnGame = document.querySelector('.new-game');
-let	cells = document.querySelectorAll('.cell');
-let	step = false;
-let	count = 0;
-let	circle = `<svg class="circle">
+let res = document.querySelector('.res');
+let btnGame = document.querySelector('.new-game');
+let cells = document.querySelectorAll('.cell');
+let step = false;
+let count = 0;
+let circle = `<svg class="circle">
 				<circle r="45" cx="58" cy="58" stroke="blue" stroke-width="10" fill="none" stroke-linecap="round" />
 			</svg>`;
-let	cross = `<svg class="cross">
+let cross = `<svg class="cross">
 				<line class="first" x1="15" y1="15" x2="100" y2="100" stroke="red" stroke-width="10" stroke-linecap="round" />
 				<line class="second" x1="100" y1="15" x2="15" y2="100" stroke="red" stroke-width="10" stroke-linecap="round" />
 			</svg>`;
